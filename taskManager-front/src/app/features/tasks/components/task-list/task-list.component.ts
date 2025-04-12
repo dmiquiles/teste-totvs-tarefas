604c8bc7fdb119932cfa56c0d631436277cdf7b1
Daniel Miquiles <danielvictormiquiles@gmail.com>
2025-04-11 22:48:44 -0300

    feat: criou componente que lista as tarefas

--- taskManager-front/src/app/features/tasks/components/task-list/task-list.component.ts
+++ taskManager-front/src/app/features/tasks/components/task-list/task-list.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Task } from '../../models/task.model';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
+
+  @Input() tasks!: Task[] | null;
+  @Output() taskCompleted = new EventEmitter<Task>();
+  filteredTasks$: Observable<any[]> = new Observable();
+  private searchTerm$ = new BehaviorSubject<string>('');
+
+  constructor(private store: Store<{ search: { term: string } }>) {
+    this.store.select('search').subscribe((state) => {
+      this.searchTerm$.next(state.term.toLowerCase());
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['tasks'] && this.tasks) {
+      this.filteredTasks$ = this.searchTerm$.pipe(
+        map((term) =>
+          (this.tasks || []).filter((task) =>
+            task.title.toLowerCase().includes(term)
+          )
+        )
+      );
+    }
+  }
+
+  toggleComplete(task: Task) {
+    task.completed = !task.completed;
+    this.taskCompleted.emit(task);
+  }
+
+  normalizePriority(task: Task): string {
+    return task.priority.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()
+  }
 
 }
