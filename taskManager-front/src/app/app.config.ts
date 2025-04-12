f785ccf296024de42e7957f56337e78b53b541f6
Daniel Miquiles <danielvictormiquiles@gmail.com>
2025-04-11 22:42:23 -0300

    feat: realizou configuração no projeto

--- taskManager-front/src/app/app.config.ts
+++ taskManager-front/src/app/app.config.ts
@@ -2,7 +2,24 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { themeReducer } from './features/tasks/store/reducers/theme.reducer';
+import { searchReducer } from './features/tasks/store/reducers/search.reducer';
+import { provideHttpClient } from '@angular/common/http';
+import { taskReducer } from './features/tasks/store/reducers/task.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { TaskEffects } from './features/tasks/store/effects/task.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore({
+      theme: themeReducer,
+      search: searchReducer,
+      tasks: taskReducer
+    }),
+    provideEffects([TaskEffects])
+  ]
 };
--- taskManager-front/src/app/features/tasks/task.routes.ts
+++ taskManager-front/src/app/features/tasks/task.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const TASK_ROUTES: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/task-page/task-page.component').then(c => c.TaskPageComponent)
+    },
+    {
+        path: 'new',
+        loadComponent: () => import('./pages/create-task-page/create-task-page.component').then(c => c.CreateTaskPageComponent)
+    },
+
+];
